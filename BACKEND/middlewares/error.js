2802d70120c19932ccbf170aaabc87c0588ed54b
K Sai Kumar <saikumarkudurupaka1610@gmail.com>
2026-04-02 11:35:09 +0530

    Refactor error middleware and remove unused code

--- BACKEND/middlewares/error.js
+++ BACKEND/middlewares/error.js
@@ -5,26 +5,16 @@ class ErrorHandler extends Error {
   }
 }
 
+export default ErrorHandler;
+
 export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
-  if (err.name === "CastError") {
-    const message = `Resource not found. Invalid: ${err.path}`;
-    err = new ErrorHandler(message, 400);
-  }
+  console.log("ERROR MIDDLEWARE:", err);
 
-
-  if (err.name === 'ValidationError') {
-    const validationErrors = Object.values(error.errors).map(err => err.message);
-    return next(new ErrorHandler(validationErrors.join(', '), 400));
-  }
-
-
-  return res.status(err.statusCode).json({
+  res.status(err.statusCode).json({
     success: false,
     message: err.message,
   });
 };
-
-export default ErrorHandler;
