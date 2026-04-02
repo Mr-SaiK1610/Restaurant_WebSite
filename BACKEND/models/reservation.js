6b70bb0ff759d68fe4513db2a0075b07b3a07757
K Sai Kumar <saikumarkudurupaka1610@gmail.com>
2026-04-02 11:34:41 +0530

    Update reservation.js

--- BACKEND/models/reservation.js
+++ BACKEND/models/reservation.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
-import validator from "validator";
 
 const reservationSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
-    minLength: [3, "First name must be of at least 3 Characters."],
-    maxLength: [30, "First name cannot exceed 30 Characters."],
   },
   lastName: {
     type: String,
     required: true,
-    minLength: [3, "Last name must be of at least 3 Characters."],
-    maxLength: [30, "Last name cannot exceed 30 Characters."],
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String, // ✅ IMPORTANT (not Number)
+    required: true,
   },
   date: {
     type: String,
@@ -21,17 +24,6 @@ const reservationSchema = new mongoose.Schema({
   time: {
     type: String,
     required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    validate: [validator.isEmail, "Provide a valid email"],
-  },
-  phone: {
-    type: String,
-    required: true,
-    minLength: [11, "Phone number must contain 11 Digits."],
-    maxLength: [11, "Phone number must contain 11 Digits."],
   },
 });
 
